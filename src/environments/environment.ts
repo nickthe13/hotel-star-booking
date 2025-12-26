725f69ff1fb6a1fef5a572c0251fdae9ebef8004
nickthe13 <nikospapageorgiou199@gmail.com>
2025-12-26 18:43:32 +0200

    Fix Stripe test key in production environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.hotel-booking.com/api',
   apiVersion: 'v1',
-  stripePublishableKey: 'pk_live_YOUR_STRIPE_LIVE_PUBLISHABLE_KEY'
+  stripePublishableKey: 'pk_test_51SidXjKSmvdwgtbdKHPDIoQjqNbNTDyLgfnp2zcGOrgxzbX5O8g5PtpsJn3uIQERXZohHKskm1ArDR2UF8wYBaWT00feXgkdwq'
 };
