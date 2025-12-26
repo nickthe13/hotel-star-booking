a184f3b354731bd66b7b3e746b130a8b900d0a70
nickthe13 <nikospapageorgiou199@gmail.com>
2025-12-26 17:37:08 +0200

    Add Stripe publishable key to environment config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/api',
-  apiVersion: 'v1'
+  apiVersion: 'v1',
+  stripePublishableKey: 'pk_test_YOUR_STRIPE_TEST_PUBLISHABLE_KEY'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.hotel-booking.com/api',
-  apiVersion: 'v1'
+  apiVersion: 'v1',
+  stripePublishableKey: 'pk_live_YOUR_STRIPE_LIVE_PUBLISHABLE_KEY'
 };
