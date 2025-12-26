c26f489ef0bab5a1296ba7bab5045d08834a8f3c
nickthe13 <nikospapageorgiou199@gmail.com>
2025-12-26 18:20:58 +0200

    Add Stripe test publishable key

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/api',
   apiVersion: 'v1',
-  stripePublishableKey: 'pk_test_YOUR_STRIPE_TEST_PUBLISHABLE_KEY'
+  stripePublishableKey: 'pk_test_51SidXjKSmvdwgtbdKHPDIoQjqNbNTDyLgfnp2zcGOrgxzbX5O8g5PtpsJn3uIQERXZohHKskm1ArDR2UF8wYBaWT00feXgkdwq'
 };
